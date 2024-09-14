125414b870667fbd343379991d3a996cd75a0b0f
Vale-Nova <valentina.nova@case.edu>
2024-09-14 16:25:21 -0400

    Menu tabs changes

--- vetero/src/components/PageHeader.vue
+++ vetero/src/components/PageHeader.vue
@@ -1,6 +1,19 @@
 <template>
     <header class="header">
       <div class="heading">Vetero</div>
+      <div class="menu-container">
+        <div class="menu">
+          <p
+            :class="{ active: selectedTab === 'Outfit of the day' }"
+            @click="selectContent('Outfit of the day')">Outfit of the day</p>
+          <p 
+            :class="{ active: selectedTab === 'Wardrobe' }"
+            @click="selectContent('Wardrobe')">Wardrobe</p>
+          <p 
+            :class="{ active: selectedTab === 'Feedback' }"
+            @click="selectContent('Feedback')">Feedback</p>
+        </div>
+      </div>
       <div class="user-profile">
         <img class="profile-pic" src="https://via.placeholder.com/50" alt="User Profile">
       </div>
@@ -8,7 +21,18 @@
   </template>
   
   <script>
+ 
   export default {
+    data() {
+    return {
+      selectedTab: 'Outfit of the day'
+    };
+  },
+  methods: {
+    selectContent(tab) {
+      this.selectedTab = tab;
+    }
+  },
     name: 'PageHeader'
   };
   </script>
@@ -32,5 +56,35 @@
     height: 50px;
     border-radius: 50%;
   }
+
+  .menu-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.menu {
+  width: 66.66%;
+  display: flex;
+  justify-content: space-around;
+  background-color: #f0f0f0;
+  padding: 10px;
+}
+
+.menu p {
+  cursor: pointer;
+  margin: 0;
+  padding: 10px 20px;
+  color: #333;
+}
+
+.menu p:hover {
+  background-color: #e0e0e0;
+}
+
+.menu p.active {
+  font-weight: bold;
+  border-bottom: 2px solid #000;
+}
   </style>
   
--- vetero/src/components/TabsMenu.vue
+++ vetero/src/components/TabsMenu.vue
@@ -1,36 +1,57 @@
 <template>
+  <div>
     <div class="menu">
-      <ul>
-        <li @click="selectContent('Outfit of the day')">Outfit of the day</li>
-        <li @click="selectContent('Wardrobe')">Wardrobe</li>
-        <li @click="selectContent('Feedback')">Feedback</li>
-      </ul>
+      <p 
+        :class="{ active: selectedTab === 'Outfit of the day' }"
+        @click="selectContent('Outfit of the day')">Outfit of the day</p>
+      <p 
+        :class="{ active: selectedTab === 'Wardrobe' }"
+        @click="selectContent('Wardrobe')">Wardrobe</p>
+      <p 
+        :class="{ active: selectedTab === 'Feedback' }"
+        @click="selectContent('Feedback')">Feedback</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      selectContent(content) {
-        this.$emit('menu-selected', content);
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedTab: 'Outfit of the day'
+    };
+  },
+  methods: {
+    selectContent(tab) {
+      this.selectedTab = tab;
     }
-  };
-  </script>
-  
-  <style scoped>
-  .menu ul {
-    list-style: none;
-    padding: 0;
   }
-  
-  .menu li {
-    cursor: pointer;
-    padding: 10px 0;
-  }
-  
-  .menu li:hover {
-    text-decoration: underline;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.menu {
+  display: flex;
+  justify-content: space-around;
+  background-color: #f0f0f0;
+  padding: 10px;
+  border-bottom: 2px solid #ccc;
+}
+
+.menu p {
+  cursor: pointer;
+  margin: 0;
+  padding: 10px 20px;
+  color: #333;
+}
+
+.menu p:hover {
+  background-color: #e0e0e0;
+}
+
+.menu p.active {
+  font-weight: bold;
+  border-bottom: 2px solid #000;
+}
+
+</style>
